a3dbfa75c40f7967e61ffacb2363846e07b1ac4c
jorgendo43 <jorge.verduzco247253@potros.itson.edu.mx>
2025-03-22 18:35:43 -0700

    Data class user
    se agrega para el futuro, cuando usemos firebase

---
+++ app/src/main/java/com/example/closetvirtual/Usuario.kt
@@ -0,0 +1,9 @@
+package com.example.closetvirtual
+
+data class Usuario(
+    val nombres: String,
+    val apellidos: String,
+    val email: String,
+    val usuario: String,
+    val contraseña: String
+)
